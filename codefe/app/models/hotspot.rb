674e4882c3d8b06dc299274a3809e46d7090ab27
pjwl33 <paul.leejw@gmail.com>
2014-03-29 18:04:30 -0400

    css workshop folder

--- codefe/app/models/hotspot.rb
+++ codefe/app/models/hotspot.rb
@@ -2,9 +2,8 @@ class Hotspot < ActiveRecord::Base
   has_many :comments
   has_and_belongs_to_many :users
 
-  # validates :name, :address, :hood, :biz_url, :type, :noise_level, presence: true
-  # validates :power, :status, inclusion: { in: [true, false] }
-  # validates :dl_speed, :ul_speed, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, :address, presence: true
+  validates :wifi_type, inclusion: { in: [true, false] }
   # validates :yelp_rating, numericality: { greater_than_or_equal_to: 0 }
 
   # @@foursquare_url = "https://api.foursquare.com/v2/venues/search?client_id=HTE5QZE5G1GZHPFBSALDZPQ5DP1YCTOZBOHY1024QST4AUEC&client_secret=NTF2HTV4X4IN5QB4ZSOUVSEQV4I4K1KI5D0MS3P4PLB3EAUP&v=20130815&near=new+york&query=coffee&intent=browse&limit=10"
