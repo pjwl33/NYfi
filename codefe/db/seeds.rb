be47c5eaf286b2cd44d7ee010c4fb7665b88910a
pjwl33 <paul.leejw@gmail.com>
2014-04-08 00:37:53 -0400

    seed file moved

--- codefe/db/seeds.rb
+++ codefe/db/seeds.rb
@@ -1,17 +1,18 @@
-# # def seeder
+user1 = User.create name: "Paul Lee", email: "pauly0418@gmail.com", password: "hello", password_confirmation: "hello", admin: true
+user1 = User.create name: "Luke Lee", email: "luke.lee@gmail.com", password: "hello", password_confirmation: "hello", admin: false
 
-# #   return wifi_data
-# # end
+require_relative './seed_data.rb'
+seed_data = seeder
 
-# # seeder.each do |hotspot|
-# #   Hotspot.create({
-# #     name: hotspot[1],
-# #     address: "#{hotspot[2]}, #{hotspot[3]}, #{hotspot[7]}",
-# #     biz_url: hotspot[4],
-# #     wifi_type: hotspot[6],
-# #     phone: hotspot[5]
-# #    })
-# # end
+seed_data.each do |hotspot|
+  Hotspot.create({
+    name: hotspot[1],
+    address: "#{hotspot[2]}, #{hotspot[3]}, #{hotspot[7]}",
+    biz_url: hotspot[4],
+    wifi_type: hotspot[6],
+    phone: hotspot[5]
+   })
+end
 
 # Hotspot.all.each do |hs|
 #   hs.update({
